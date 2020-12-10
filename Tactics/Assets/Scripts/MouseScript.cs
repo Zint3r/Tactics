4d98b9591d8c77f0ce0844ccf71cba6f2f61b9c9
Zint3r <48679829+Zint3r@users.noreply.github.com>
2020-12-11 02:08:00 +0300

    Add new script

--- Tactics/Assets/Scripts/MouseScript.cs
+++ Tactics/Assets/Scripts/MouseScript.cs
@@ -6,7 +6,7 @@ public class MouseScript : MonoBehaviour
     private ParticleSystem _onClickEffect;
     private PlayerControls _input;
     private NavMeshAgent _navMesh;
-    private Vector3 _movePoint;
+    private Vector3 _movePoint;   
     private void Awake()
     {
         _input = new PlayerControls();
@@ -25,34 +25,53 @@ public class MouseScript : MonoBehaviour
         _onClickEffect = _onClickObj.GetComponentInChildren<ParticleSystem>();
         _navMesh = GetComponent<NavMeshAgent>();
     }
-    private void Update()
+    private void LateUpdate()
     {
-        _navMesh.SetDestination(_movePoint);
+        
     }
     private void OnClick()
-    {
-        //RaycastHit hit;
-        //Vector3 clickPosition = new Vector3(_input.Player.Clickposition.ReadValue<Vector2>().x, _input.Player.Clickposition.ReadValue<Vector2>().y, 0);
-        ////Debug.Log(_input.Player.Clickposition.ReadValue<Vector2>());
-        //Ray ray = Camera.main.ScreenPointToRay(clickPosition);
-        //if (Physics.Raycast(ray, out hit))
-        //{
-        //    Vector3 newPos = Camera.main.ScreenToWorldPoint(hit.point);
-        //    _onClickObj.transform.position = newPos;
-        //    _onClickEffect.Play();
-        //    _movePoint = clickPosition;           
-        //}
-
-
-
+    {      
         Ray camRay = Camera.main.ScreenPointToRay(_input.Player.Clickposition.ReadValue<Vector2>());
         RaycastHit hit;
 
         if (Physics.Raycast(camRay, out hit))
         {
-            _onClickObj.transform.position = hit.point;
-            _onClickEffect.Play();
-            _movePoint = hit.point;
+            if (hit.collider.gameObject.layer == 0)
+            {
+                _onClickObj.transform.position = hit.point;
+                _onClickEffect.Play();
+                _movePoint = hit.point;
+                _navMesh.SetDestination(GetCharacterPositionOnNavMesh(_movePoint));
+            }                     
+        }
+    }
+    public Vector3 GetCharacterPositionOnNavMesh(Vector3 position)
+    {
+        NavMeshHit hit;
+        bool positionFound = NavMesh.SamplePosition(position, out hit, 50, NavMesh.AllAreas);
+
+        if (!positionFound)
+            Debug.LogWarning("No valid position found !");
+
+        if (positionFound == true)
+        {
+            return hit.position;
+        }
+        else
+        {
+            return position;
+        }        
+    }
+    private void RayCaster()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, 10f))
+        {
+            if (hit.collider.gameObject.GetComponent<DoorScript>() == true)
+            {
+                _navMesh.ResetPath();                
+            }
         }
     }
 }
